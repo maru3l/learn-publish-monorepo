b8ed517586647b4bf2995679753f6bdd370e3684
Marc-Antoine Ruel <hello@marcantoineruel.com>
2020-04-02 12:19:06 -0400

    feat: add style to input

--- packages/shared-ui/src/Input/Input.js
+++ packages/shared-ui/src/Input/Input.js
@@ -1,4 +1,13 @@
 // vendors
 import React from 'react';
+import styled from 'styled-components';
 
-export default props => (<input {...props}/>)
+const StyledInput = styled.input`
+  appearance: none;
+  border: 1px solid tomato;
+  border-radius: 0;
+  background: inherit;
+  color: tomato;
+`;
+
+export default props => (<StyledInput {...props}/>)
